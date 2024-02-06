0ddc4b44fb56dfa7b0a610353d6d93835024621c
EdwinUnity3D <eapaniagua8@misena.edu.co>
2024-02-06 14:11:38 -0500

    movimiento personaje con limites al movimiento horizontal

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using Vector3 = System.Numerics.Vector3;
 
 public class PlayerController : MonoBehaviour
 
 {
-
-    [SerializeField, Range(-200, 200)] private float speed = 20;
+    [SerializeField, Range(-200, 200), Tooltip("Velocidad de player")]
+    private float speed = 12;
 
     private float horizontal, vertical;
+
+    [SerializeField, Range(-17, 17), Tooltip("Limite movimiento en X del player")]
+    private float xRrange;
+    
+    
     
     
     // Start is called before the first frame update
@@ -22,9 +28,27 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");  
-        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontal);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * vertical);
+        vertical = Input.GetAxis("Vertical");
+        
+        
+      transform.Translate(UnityEngine.Vector3.right * speed * Time.deltaTime * horizontal );
+      transform.Translate(UnityEngine.Vector3.forward * speed * Time.deltaTime * vertical);
 
+
+      if (transform.position.x < -xRrange)
+      {
+          transform.position = new UnityEngine.Vector3(-xRrange, transform.position.y, transform.position.z);
+          
+      }
+
+      if (transform.position.x > xRrange)
+      {
+          transform.position = new UnityEngine.Vector3(xRrange, transform.position.y, transform.position.z);
+      }
+      
     }
+    
+    
+    
+    
 }
