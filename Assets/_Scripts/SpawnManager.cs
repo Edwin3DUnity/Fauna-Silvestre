7c1f05b151c419bbf5bb2826c78fcb0b7c4da664
EdwinUnity3D <eapaniagua8@misena.edu.co>
2024-02-07 14:12:27 -0500

    Se modificó script de spawn Manager

--- Assets/_Scripts/SpawnManager.cs
+++ Assets/_Scripts/SpawnManager.cs
@@ -1,21 +1,46 @@
+
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using Random = UnityEngine.Random;
+
+
+
 public class SpawnManager : MonoBehaviour
 {
-
-
     public GameObject[] enemies;
-    // Start is called before the first frame update
-    void Start()
+    private int arrayIndex;
+
+    public float spawnPosX = 20;
+    private float spawnPosZ;
+
+    private void Start()
     {
-        
+        spawnPosZ = transform.position.z;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            float xRange = Random.Range(-spawnPosX, spawnPosX);
+
+            Vector3 randomPosition = new Vector3(xRange, 0, spawnPosZ);
+
+            arrayIndex = Random.Range(0, enemies.Length);
+
+
+            Instantiate(enemies[arrayIndex], randomPosition, enemies[arrayIndex].transform.rotation);
+
+
+
+
+
+
+        }
     }
 }
